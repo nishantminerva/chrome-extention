ded7d355ea4d8c2b2075f72a38ea3c28bf243b90
Nishant Minerva <nishantcafe@gmail.com>
2023-03-13 18:08:50 +0530

    learning to create browser extentions

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+// Node 14 & 16
+// import manifest from './manifest.json'
+// Node >=17
+import manifest from './manifest.json' assert { type: 'json' } 
+
+export default defineConfig({
+  plugins: [react(), crx({ manifest })],
+});
